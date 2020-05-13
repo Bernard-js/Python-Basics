6e682c491bf2a7ed6e9fe59755ed48016db2e878
Bernard-js <59395325+Bernard-js@users.noreply.github.com>
2020-05-13 00:17:54 -0300

    updating examples of module imports

--- ImportingFiles/import.py
+++ ImportingFiles/import.py
@@ -1,1 +1,16 @@
+# importing a module that i've created
+import instances
 
+# importing a built-in python module
+import fractions
+
+# importing an external module that i downloaded using pip package manager
+import docx
+
+print(instances.band_members)
+print(instances.kilometers_to_meters(2))
+
+print(fractions.Fraction(10, 11))
+print(fractions.Fraction(42, 12))
+
+print(docx.ImagePart)
--- ImportingFiles/instances.py
+++ ImportingFiles/instances.py
@@ -3,9 +3,11 @@ prime_numbers = [2, 3, 5, 7, 11, 13, 17]
 
 band_members = ['corey', 'shawn', 'mick', 'sid']
 
+
 def kilometers_to_meters(number):
     number = number * 1000
     return number
+
 
 def liters_to_cubic_meters(number):
     number = number / 1000
