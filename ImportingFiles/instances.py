5c96452a52530ef1539dd31e9e30ffd919776ec3
Bernard-js <59395325+Bernard-js@users.noreply.github.com>
2020-05-12 22:04:03 -0300

    importing files in python

---
+++ ImportingFiles/instances.py
@@ -0,0 +1,12 @@
+
+prime_numbers = [2, 3, 5, 7, 11, 13, 17]
+
+band_members = ['corey', 'shawn', 'mick', 'sid']
+
+def kilometers_to_meters(number):
+    number = number * 1000
+    return number
+
+def liters_to_cubic_meters(number):
+    number = number / 1000
+    return number
